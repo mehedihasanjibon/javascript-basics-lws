98f2f27aa776b69d6587e61fbb3be0f8d6f6341a
Mehedi Hasan <mhjibon.info@gmail.com>
2023-05-09 00:46:03 +0600

    js can hide html element and can show the

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ document.getElementById("demo").innerHTML = "Hello Javascript";
 function myFunction(){
     document.getElementById("demo2").style.fontSize = "45px";
 }
+
+function myFunction2(){
+    document.getElementById("demo3").style.display ="none";
+}
+
+function myBtn(){
+    document.getElementById("demo4").style.display="block";
+}
+
+function myBtn2(){
+    document.getElementById("demo5").innerHTML = "paragraph change";
+}
